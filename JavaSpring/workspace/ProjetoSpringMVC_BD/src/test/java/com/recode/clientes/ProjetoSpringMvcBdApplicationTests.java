a172183489a6f66ca10c5f0937c455900ab124b2
marcosppsilva <criativapixel@gmail.com>
2023-11-17 18:49:16 +0000

    java

---
+++ JavaSpring/workspace/ProjetoSpringMVC_BD/src/test/java/com/recode/clientes/ProjetoSpringMvcBdApplicationTests.java
@@ -0,0 +1,13 @@
+package com.recode.clientes;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjetoSpringMvcBdApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
