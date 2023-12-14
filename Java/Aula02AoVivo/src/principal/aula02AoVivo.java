329aeb5d2e55a2bf15397775619b2adfc04dd53b
marcosppsilva <criativapixel@gmail.com>
2023-12-14 15:15:55 +0000

    Java

--- Java/Aula02AoVivo/src/principal/aula02AoVivo.java
+++ Java/Aula02AoVivo/src/principal/aula02AoVivo.java
@@ -10,8 +10,11 @@ public class aula02AoVivo {
 
 		double minutos, valorFinal, base;
 		base = 50;
+		
+		int numFuncionario, horasTrab;
+		double valorHora, salarioFinal;
 
-		System.out.println("Quantos minutos foram consumidos?");
+		/*System.out.println("Quantos minutos foram consumidos?");
 		minutos = teclado.nextDouble();
 
 		if (minutos > 100) {
@@ -19,7 +22,20 @@ public class aula02AoVivo {
 			System.out.println("O valor de conta a pagar será R$" + valorFinal);
 		} else {
 			System.out.println("O valor de conta a pagar será R$" + base);
-		}
+		}*/
+		
+		System.out.println(10 > 5 * 2);
+		
+		System.out.println("Informe o numero do funcionário");
+		numFuncionario = teclado.nextInt();
+		System.out.println("Informe as horas trabalhadas pelo funcionário no mês");
+		horasTrab = teclado.nextInt();
+		System.out.println("Informe o valor por hora trabalhada do funcionário");
+		valorHora = teclado.nextDouble();
+		
+		salarioFinal = horasTrab * valorHora;
+		
+		System.out.println("O funcionário " + numFuncionario + " no mês atual trabalhou " + horasTrab + " o valor da sua hora é " + valorHora + " e seu salário final foi de R$ %.2f%" + salarioFinal);
 
 		teclado.close();
 	}
